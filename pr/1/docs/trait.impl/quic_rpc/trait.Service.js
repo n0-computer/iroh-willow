28806263e05d4e25ceaa3e1e06385c503b318f5a
matheus23 <matheus23@users.noreply.github.com>
2024-11-14 12:50:43 +0000

    deploy: 9fce83a1d7ba94a36e00117a998718cecbdd7bd6

---
+++ pr/1/docs/trait.impl/quic_rpc/trait.Service.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_willow":[["impl Service for <a class=\"struct\" href=\"iroh_willow/rpc/proto/struct.RpcService.html\" title=\"struct iroh_willow::rpc::proto::RpcService\">RpcService</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
