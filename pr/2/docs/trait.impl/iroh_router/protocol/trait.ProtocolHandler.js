942ef11872d825c46b3e4e811dd502873bc45da9
matheus23 <matheus23@users.noreply.github.com>
2024-11-15 14:46:50 +0000

    deploy: 248967ec3b021b2b76f22fbfc7b7e61e5055ad42

---
+++ pr/2/docs/trait.impl/iroh_router/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_willow":[["impl ProtocolHandler for <a class=\"struct\" href=\"iroh_willow/engine/struct.Engine.html\" title=\"struct iroh_willow::engine::Engine\">Engine</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ pr/2/docs/trait.impl/tokio/io/async_write/trait.AsyncWrite.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_willow":[["impl AsyncWrite for <a class=\"struct\" href=\"iroh_willow/util/channel/struct.Writer.html\" title=\"struct iroh_willow::util::channel::Writer\">Writer</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
