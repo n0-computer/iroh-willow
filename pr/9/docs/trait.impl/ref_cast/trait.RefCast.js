84436b1038d17631d58b11296f7934a5a93db285
matheus23 <matheus23@users.noreply.github.com>
2025-01-13 08:51:26 +0000

    deploy: ec826a2190b996cf88c43b8dceb2f90ccf5519e6

---
+++ pr/9/docs/trait.impl/ref_cast/trait.RefCast.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_willow",[["impl&lt;C: Connector&lt;<a class=\"struct\" href=\"iroh_willow/rpc/proto/struct.RpcService.html\" title=\"struct iroh_willow::rpc::proto::RpcService\">RpcService</a>&gt;&gt; <a class=\"trait\" href=\"https://docs.rs/ref-cast/1.0.23/ref_cast/trait.RefCast.html\" title=\"trait ref_cast::RefCast\">RefCast</a> for <a class=\"struct\" href=\"iroh_willow/rpc/client/struct.Client.html\" title=\"struct iroh_willow::rpc::client::Client\">Client</a>&lt;C&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[474]}
---
+++ pr/9/docs/trait.impl/willow_data_model/parameters/trait.SubspaceId.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_willow",[["impl SubspaceId for <a class=\"struct\" href=\"iroh_willow/proto/keys/struct.UserId.html\" title=\"struct iroh_willow::proto::keys::UserId\">UserId</a>"],["impl SubspaceId for <a class=\"struct\" href=\"iroh_willow/proto/keys/struct.UserPublicKey.html\" title=\"struct iroh_willow::proto::keys::UserPublicKey\">UserPublicKey</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[350]}
